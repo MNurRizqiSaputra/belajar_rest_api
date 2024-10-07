6fb8e23d7954402bc0227bd3c5eb3d2e0639596d
MNurRizqiSaputra <putra15x@gmail.com>
2024-10-07 12:28:37 +0700

    Rest API : 3.Logger Pino.js & Handle Request Body

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import express, { Application } from "express";
 import { routes } from "./routes";
+import { logger } from "./utils/logger";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app: Application = express();
 const port: Number = 4000;
 
+// parsing request body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// cors access handler
+app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+})
+
 routes(app);
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    logger.info(`Server is running at http://localhost:${port}`);
 })
